c89e0f9445268ad1fbbd655ceea59dedd489d55b
TahiryHenitsoa <118928190+TahiryRamaroson@users.noreply.github.com>
2024-01-12 00:57:55 +0300

    Modification login

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -1,25 +1,52 @@
 import {
-  Card,
   Input,
-  Checkbox,
   Button,
   Typography,
 } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function SignIn() {
+  const navigate = useNavigate();
+
+  // État local pour suivre les valeurs du formulaire
+  const [formData, setFormData] = useState({
+    mdp: '',
+    email: '',
+  });
+
+  // Gestionnaire d'événement pour la saisie des champs
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // Gestionnaire d'événement pour la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Vous pouvez faire quelque chose avec les données ici
+    console.log('Données soumises :', formData);
+    navigate('/dashboard/home');
+  };
+
   return (
     <section className="m-8 flex gap-4">
       <div className="w-full lg:w-3/5 mt-24">
         <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Sign In</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
+          <img src="/img/Occasion360_logo.png" height={150} width={150} alt="" style={{ margin: 'auto' }}/>
+          <br/>
+          <Typography variant="h2" className="font-bold mb-4">Login</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">connectez-vous à occasion360</Typography>
         </div>
-        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
+        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2"  
+              onSubmit={handleSubmit}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Your email
+              Votre email
             </Typography>
             <Input
               size="lg"
@@ -28,9 +55,13 @@ export function SignIn() {
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
             />
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-              Password
+              Mot de passe
             </Typography>
             <Input
               type="password"
@@ -40,49 +71,15 @@ export function SignIn() {
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
+              name="mdp"
+              value={formData.mdp}
+              onChange={handleChange}
             />
           </div>
-          <Checkbox
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center justify-start font-medium"
-              >
-                I agree the&nbsp;
-                <a
-                  href="#"
-                  className="font-normal text-black transition-colors hover:text-gray-900 underline"
-                >
-                  Terms and Conditions
-                </a>
-              </Typography>
-            }
-            containerProps={{ className: "-ml-2.5" }}
-          />
-          <Button className="mt-6" fullWidth>
+          <Button className="mt-6" fullWidth type="submit">
             Sign In
           </Button>
 
-          <div className="flex items-center justify-between gap-2 mt-6">
-            <Checkbox
-              label={
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center justify-start font-medium"
-                >
-                  Subscribe me to newsletter
-                </Typography>
-              }
-              containerProps={{ className: "-ml-2.5" }}
-            />
-            <Typography variant="small" className="font-medium text-gray-900">
-              <a href="#">
-                Forgot Password
-              </a>
-            </Typography>
-          </div>
           <div className="space-y-4 mt-8">
             <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
               <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -105,10 +102,6 @@ export function SignIn() {
               <span>Sign in With Twitter</span>
             </Button>
           </div>
-          <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-            Not registered?
-            <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
-          </Typography>
         </form>
 
       </div>
